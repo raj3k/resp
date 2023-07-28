a990484e479333a330b714d7339d9fa103b75515
raj3k <rajkosky@gmail.com>
2023-07-28 15:57:13 +0200

    test refactoring

--- serialize_test.go
+++ serialize_test.go
@@ -1,92 +1,120 @@
 package resp
 
 import (
-	"fmt"
 	"testing"
 )
 
-func TestSerializeRESP(t *testing.T) {
-	testData := []struct {
-		input    *RESPValue
-		expected string
-	}{
-		{
-			input: &RESPValue{
-				Type: SimpleString,
-				Data: []byte("OK"),
-			},
-			expected: "+OK\r\n",
-		},
-		{
-			input: &RESPValue{
-				Type: Error,
-				Data: []byte("Error message"),
-			},
-			expected: "-Error message\r\n",
-		},
-		{
-			input: &RESPValue{
-				Type: Integer,
-				Data: []byte("42"),
-			},
-			expected: ":42\r\n",
-		},
-		{
-			input: &RESPValue{
-				Type: BulkString,
-				Data: []byte("Hello, World!"),
-			},
-			expected: "$13\r\nHello, World!\r\n",
-		},
-		{
-			input: &RESPValue{
-				Type: Array,
-				ArrayElements: []*RESPValue{
-					{Type: BulkString, Data: []byte("foo")},
-					{Type: BulkString, Data: []byte("bar")},
-				},
-			},
-			expected: "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n",
-		},
-		{
-			input: &RESPValue{
-				Type: Array,
-			},
-			expected: "*-1\r\n",
-		},
-		{
-			input: &RESPValue{
-				Type: BulkString,
-			},
-			expected: "$-1\r\n",
-		},
-		{
-			input: &RESPValue{
-				Type: Array,
-				ArrayElements: []*RESPValue{
-					{
-						Type: BulkString,
-						Data: []byte("hello"),
-					},
-					{
-						Type: BulkString,
-					},
-					{
-						Type: BulkString,
-						Data: []byte("world"),
-					},
-				},
-			},
-			expected: "*3\r\n$5\r\nhello\r\n$-1\r\n$5\r\nworld\r\n",
-		},
+// Test serialization for SimpleString
+func TestSerializeSimpleString(t *testing.T) {
+	value := &SimpleString{Data: []byte("OK")}
+	expected := "+OK\r\n"
+
+	serialized, err := Encode(value)
+	if err != nil {
+		t.Errorf("Error during serialization: %v", err)
+		return
 	}
 
-	for _, td := range testData {
-		serializedData := SerializeRESP(td.input)
-		if serializedData != td.expected {
-			fmt.Printf("SerializeRESP(%q) - Test Failed:\nExpected: %q\nGot: %q\n", td.input.Type, td.expected, string(serializedData))
-		} else {
-			fmt.Printf("SerializeRESP(%q) - Test Passed\n", td.input.Type)
-		}
+	if string(serialized) != expected {
+		t.Errorf("Serialization failed. Expected: %q, Got: %q", expected, string(serialized))
+	}
+}
+
+// Test serialization for Error
+func TestSerializeError(t *testing.T) {
+	value := &Error{Data: []byte("Error message")}
+	expected := "-Error message\r\n"
+
+	serialized, err := Encode(value)
+	if err != nil {
+		t.Errorf("Error during serialization: %v", err)
+		return
+	}
+
+	if string(serialized) != expected {
+		t.Errorf("Serialization failed. Expected: %q, Got: %q", expected, string(serialized))
+	}
+}
+
+// Test serialization for Integer
+func TestSerializeInteger(t *testing.T) {
+	value := &Integer{Data: []byte("42")}
+	expected := ":42\r\n"
+
+	serialized, err := Encode(value)
+	if err != nil {
+		t.Errorf("Error during serialization: %v", err)
+		return
+	}
+
+	if string(serialized) != expected {
+		t.Errorf("Serialization failed. Expected: %q, Got: %q", expected, string(serialized))
+	}
+}
+
+// Test serialization for BulkString
+func TestSerializeBulkString(t *testing.T) {
+	value := &BulkString{Data: []byte("Hello, World!")}
+	expected := "$13\r\nHello, World!\r\n"
+
+	serialized, err := Encode(value)
+	if err != nil {
+		t.Errorf("Error during serialization: %v", err)
+		return
+	}
+
+	if string(serialized) != expected {
+		t.Errorf("Serialization failed. Expected: %q, Got: %q", expected, string(serialized))
+	}
+}
+
+func TestSerializeBulkStringNull(t *testing.T) {
+	value := &BulkString{Data: nil}
+	expected := "$-1\r\n"
+
+	serialized, err := Encode(value)
+	if err != nil {
+		t.Errorf("Error during serialization: %v", err)
+		return
+	}
+
+	if string(serialized) != expected {
+		t.Errorf("Serialization failed. Expected: %q, Got: %q", expected, string(serialized))
+	}
+}
+
+// Test serialization for Array
+func TestSerializeArray(t *testing.T) {
+	value := &Array{
+		ArrayElements: []Value{
+			&BulkString{Data: []byte("foo")},
+			&BulkString{Data: []byte("bar")},
+		},
+	}
+	expected := "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"
+
+	serialized, err := Encode(value)
+	if err != nil {
+		t.Errorf("Error during serialization: %v", err)
+		return
+	}
+
+	if string(serialized) != expected {
+		t.Errorf("Serialization failed. Expected: %q, Got: %q", expected, string(serialized))
+	}
+}
+
+func TestSerializeArrayNull(t *testing.T) {
+	value := &Array{ArrayElements: nil}
+	expected := "*-1\r\n"
+
+	serialized, err := Encode(value)
+	if err != nil {
+		t.Errorf("Error during serialization: %v", err)
+		return
+	}
+
+	if string(serialized) != expected {
+		t.Errorf("Serialization failed. Expected: %q, Got: %q", expected, string(serialized))
 	}
 }
