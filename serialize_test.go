257532e7eea12adc605b6cf7eb50eed6bead69e4
raj3k <rajkosky@gmail.com>
2023-07-29 12:45:37 +0200

    small changes to existing tests and new test for deserialization of SimpleString RESP Value

--- serialize_test.go
+++ serialize_test.go
@@ -1,15 +1,18 @@
 package resp
 
 import (
+	"errors"
 	"testing"
 )
 
 // Test serialization for SimpleString
 func TestSerializeSimpleString(t *testing.T) {
+	serializer := NewRespSerializer()
+
 	value := &SimpleString{Data: []byte("OK")}
 	expected := "+OK\r\n"
 
-	serialized, err := Encode(value)
+	serialized, err := serializer.Serialize(value)
 	if err != nil {
 		t.Errorf("Error during serialization: %v", err)
 		return
@@ -22,10 +25,12 @@ func TestSerializeSimpleString(t *testing.T) {
 
 // Test serialization for Error
 func TestSerializeError(t *testing.T) {
+	serializer := NewRespSerializer()
+
 	value := &Error{Data: []byte("Error message")}
 	expected := "-Error message\r\n"
 
-	serialized, err := Encode(value)
+	serialized, err := serializer.Serialize(value)
 	if err != nil {
 		t.Errorf("Error during serialization: %v", err)
 		return
@@ -38,10 +43,12 @@ func TestSerializeError(t *testing.T) {
 
 // Test serialization for Integer
 func TestSerializeInteger(t *testing.T) {
+	serializer := NewRespSerializer()
+
 	value := &Integer{Data: []byte("42")}
 	expected := ":42\r\n"
 
-	serialized, err := Encode(value)
+	serialized, err := serializer.Serialize(value)
 	if err != nil {
 		t.Errorf("Error during serialization: %v", err)
 		return
@@ -54,10 +61,12 @@ func TestSerializeInteger(t *testing.T) {
 
 // Test serialization for BulkString
 func TestSerializeBulkString(t *testing.T) {
+	serializer := NewRespSerializer()
+
 	value := &BulkString{Data: []byte("Hello, World!")}
 	expected := "$13\r\nHello, World!\r\n"
 
-	serialized, err := Encode(value)
+	serialized, err := serializer.Serialize(value)
 	if err != nil {
 		t.Errorf("Error during serialization: %v", err)
 		return
@@ -69,10 +78,12 @@ func TestSerializeBulkString(t *testing.T) {
 }
 
 func TestSerializeBulkStringNull(t *testing.T) {
+	serializer := NewRespSerializer()
+
 	value := &BulkString{Data: nil}
 	expected := "$-1\r\n"
 
-	serialized, err := Encode(value)
+	serialized, err := serializer.Serialize(value)
 	if err != nil {
 		t.Errorf("Error during serialization: %v", err)
 		return
@@ -85,6 +96,8 @@ func TestSerializeBulkStringNull(t *testing.T) {
 
 // Test serialization for Array
 func TestSerializeArray(t *testing.T) {
+	serializer := NewRespSerializer()
+
 	value := &Array{
 		ArrayElements: []Value{
 			&BulkString{Data: []byte("foo")},
@@ -93,7 +106,7 @@ func TestSerializeArray(t *testing.T) {
 	}
 	expected := "*2\r\n$3\r\nfoo\r\n$3\r\nbar\r\n"
 
-	serialized, err := Encode(value)
+	serialized, err := serializer.Serialize(value)
 	if err != nil {
 		t.Errorf("Error during serialization: %v", err)
 		return
@@ -105,10 +118,12 @@ func TestSerializeArray(t *testing.T) {
 }
 
 func TestSerializeArrayNull(t *testing.T) {
+	serializer := NewRespSerializer()
+
 	value := &Array{ArrayElements: nil}
 	expected := "*-1\r\n"
 
-	serialized, err := Encode(value)
+	serialized, err := serializer.Serialize(value)
 	if err != nil {
 		t.Errorf("Error during serialization: %v", err)
 		return
@@ -116,5 +131,65 @@ func TestSerializeArrayNull(t *testing.T) {
 
 	if string(serialized) != expected {
 		t.Errorf("Serialization failed. Expected: %q, Got: %q", expected, string(serialized))
+	}
+}
+
+func TestDecodeSimpleString(t *testing.T) {
+	testCases := []struct {
+		input    []byte
+		expected Value
+		err      error
+	}{
+		// Valid input
+		{
+			input:    []byte("+Hello, World!\r\n"),
+			expected: &SimpleString{Data: []byte("Hello, World!")},
+			err:      nil,
+		},
+
+		// Empty input
+		{
+			input:    []byte(""),
+			expected: nil,
+			err:      errors.New("invalid SimpleString format"),
+		},
+
+		// Missing newline
+		{
+			input:    []byte("+Hello, World!\r"),
+			expected: nil,
+			err:      errors.New("invalid SimpleString format"),
+		},
+
+		// Missing "+"
+		{
+			input:    []byte("Hello, World!\r\n"),
+			expected: nil,
+			err:      errors.New("invalid SimpleString format"),
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(string(tc.input), func(t *testing.T) {
+			value, err := decodeSimpleString(tc.input)
+			if tc.err != nil {
+				if err == nil || err.Error() != tc.err.Error() {
+					t.Errorf("Expected error: %v, got: %v", tc.err, err)
+				}
+			} else {
+				if err != nil {
+					t.Errorf("Unexpected error: %v", err)
+				}
+				if value.GetType() != tc.expected.GetType() {
+					t.Errorf("Expected type: %v, got: %v", tc.expected.GetType(), value.GetType())
+				}
+				// Compare the SimpleString Data field.
+				expectedData := tc.expected.(*SimpleString).Data
+				actualData := value.(*SimpleString).Data
+				if string(expectedData) != string(actualData) {
+					t.Errorf("Expected Data: %q, got: %q", expectedData, actualData)
+				}
+			}
+		})
 	}
 }
